c57ff136335b90153bc5c4dad270eadfb19975df
HanzhiXu33 <h5xu@ucsd.edu>
2024-10-15 12:11:18 +0800

    Update tinyfoot.js

--- src/tinyfoot.js
+++ src/tinyfoot.js
@@ -1,1 +1,62 @@
-// JavaScript code for Tinyfoot will be added here.
+(function() {
+  (function($) {
+    return $.tinyfoot = function(options) {
+      var settings, footnoteInit, updateCounters, createPopover, removePopovers, repositionFeet;
+      
+      settings = $.extend({
+        actionOriginalFN: "hide",
+        allowMultipleFN: false,
+        buttonMarkup: "<div class='tinyfoot-footnote__container'><button class='tinyfoot-footnote__button' data-footnote-number='{{FOOTNOTENUM}}' data-footnote-identifier='{{FOOTNOTEID}}'>{{FOOTNOTENUM}}</button></div>",
+        contentMarkup: "<aside class='tinyfoot-footnote' data-footnote-number='{{FOOTNOTENUM}}'>{{FOOTNOTECONTENT}}</aside>",
+        popoverDeleteDelay: 300,
+        popoverCreateDelay: 100
+      }, options);
+
+      // Function to initialize footnotes
+      footnoteInit = function() {
+        // Basic initialization logic for footnotes, similar to bigfoot but minimal
+        var footnotes = $(".tinyfoot-footnote__button");
+        updateCounters(); // Update the counters
+        footnotes.on('click', function() {
+          if (!$(this).hasClass('read')) {
+            $(this).addClass('read');
+            $(this).css('background-color', 'lightgreen');
+            updateCounters(); // Recalculate counters
+          }
+        });
+      };
+
+      // Function to update counters (total and unread)
+      updateCounters = function() {
+        var totalFootnotes = $(".tinyfoot-footnote__button").length;
+        var unreadFootnotes = $(".tinyfoot-footnote__button:not(.read)").length;
+        $('#footnote-counter').text(`Total Footnotes: ${totalFootnotes}, Unread Footnotes: ${unreadFootnotes}`);
+      };
+
+      // Function to create popovers (minimalist version)
+      createPopover = function() {
+        // Logic to create footnote popovers when clicked
+      };
+
+      // Function to remove popovers
+      removePopovers = function() {
+        // Logic to remove footnotes
+      };
+
+      // Function to reposition footnotes
+      repositionFeet = function() {
+        // Logic to reposition
+      };
+
+      $(document).ready(function() {
+        footnoteInit(); // Initialize tinyfoot
+        $(window).on("resize", repositionFeet); // Handle resize
+      });
+
+      return {
+        reposition: repositionFeet,
+        removePopovers: removePopovers
+      };
+    };
+  })(jQuery);
+})();
